b777b97e9540ae3e02c77bf144c9eea1ec5e95bf
Yersin <yersinme@gmail.com>
2025-07-03 23:17:14 +0500

    init base nuxt config params

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+    modules: ['@nuxtjs/google-fonts', '@nuxtjs/device'],
+    googleFonts: {
+        families: {
+            Poppins: [400, 500, 700],
+            'Open+Sans': true,
+        }
+    },
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    css: ['@/assets/styles/main.scss'],
+    runtimeConfig: {
+        API_URL: 'https://movies-tv-shows-database.p.rapidapi.com',
+        API_KEY: 'e63934d7aamsheec2dffb4bbada1p1b81e6jsn4a88d3693a94',
+        API_HOST: 'movies-tv-shows-database.p.rapidapi.com',
+        public: {
+            apiBase: '/api'
+        }
+    }
 })
