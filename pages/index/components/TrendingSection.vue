5f010c00272571811486bb668b0da4619c5d76ee
Yersin <yersinme@gmail.com>
2025-07-04 03:50:15 +0500

    Trending fix

--- pages/index/components/TrendingSection.vue
+++ pages/index/components/TrendingSection.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
 
+const { data: movies } = await useFetch('/api/movies/trending', {
+  transform: res =>
+    res.movies.map((m: any) => ({
+      id: m.imdb_id,
+      title: m.title,
+      year: m.year,
+      backgroundImage: '/images/test.png'
+    }))
+})
+
+const handleToggleFavorite = (id: string | number) => {}
 </script>
 
 <template>
-  <UiCard
-    :id="1"
-    title="Интерстеллар"
-    year="2014"
-    background-image="/images/test.png"
-    :is-favorite="false"
-    @toggle-favorite="handleToggleFavorite"
-  />
+  <div>
+    <UiCard
+      v-for="movie in movies"
+      :key="movie.id"
+      :id="movie.id"
+      :title="movie.title"
+      :year="movie.year"
+      :background-image="movie.backgroundImage"
+      :is-favorite="false"
+      @toggle-favorite="handleToggleFavorite"
+    />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
