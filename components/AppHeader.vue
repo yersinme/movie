f78347374ec42c69d760571c42e99ff08e7adb26
Yersin <yersinme@gmail.com>
2025-07-04 22:44:59 +0500

    header fix

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { HEADER_LINKS } from '~/pages/index/constants'
+import {HEADER_LINKS} from '~/pages/index/constants'
 import SearchBar from '~/components/SearchBar.vue'
 </script>
 
@@ -7,19 +7,21 @@ import SearchBar from '~/components/SearchBar.vue'
   <header class="header">
     <div class="links">
       <nuxt-link
-        v-for="l in HEADER_LINKS"
-        :key="l.route"
-        class="link"
-        :to="l.route"
+          v-for="l in HEADER_LINKS"
+          :key="l.route"
+          class="link"
+          :to="l.route"
       >
         {{ l.title }}
       </nuxt-link>
     </div>
 
     <div class="buttons">
-      <SearchBar />
-      <img src="~/public/icons/bell.svg" alt="bell" class="icon" />
-      <img src="~/public/images/avatar.jpg" alt="avatar" class="icon" />
+      <SearchBar/>
+      <img src="~/public/icons/bell.svg" alt="bell" class="icon"/>
+      <span class="account"><img src="~/public/images/avatar.jpg" alt="avatar"
+                 class="icon"/> <p>Tetiana</p></span>
+
     </div>
   </header>
 </template>
@@ -29,20 +31,31 @@ import SearchBar from '~/components/SearchBar.vue'
   display: flex;
   justify-content: space-between;
 }
+
 .links {
   display: flex;
   gap: 24px;
 }
+
 .link {
   color: var(--white);
 }
+
 .buttons {
   display: flex;
   align-items: center;
   gap: 24px;
+
   .icon {
     cursor: pointer;
     border-radius: 50%;
+  }
+
+  .account  {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: var(--white);
   }
 }
 </style>
