7000c58850233d757a4949542f8d4cc8678e4c96
Yersin <yersinme@gmail.com>
2025-07-04 18:29:45 +0500

    add favourites store

--- stores/favourites.ts
+++ stores/favourites.ts
@@ -1,21 +1,28 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 export const useFavouritesStore = defineStore('favourites', {
-  state: () => ({
-    ids: [] as (string | number)[]
-  }),
+    state: () => ({
+        ids: [] as (string | number)[]
+    }),
 
-  actions: {
-    toggle(id: string | number) {
-      this.ids.includes(id)
-        ? (this.ids = this.ids.filter(x => x !== id))
-        : this.ids.push(id)
+    actions: {
+        initFavorites() {
+            const idxJson = localStorage.getItem('favourites');
+            this.ids = idxJson ? JSON.parse(idxJson) : [];
+            console.log('this.ids ',this.ids )
+        },
+
+        toggle(id: string | number) {
+            this.ids.includes(id)
+                ? (this.ids = this.ids.filter(x => x !== id))
+                : this.ids.push(id)
+            localStorage.setItem('favourites', JSON.stringify(this.ids))
+        },
+
+        isFavourite(id: string | number) {
+            return this.ids.includes(id)
+        }
     },
 
-    isFavourite(id: string | number) {
-      return this.ids.includes(id)
-    }
-  },
-
-  persist: true
+    persist: true
 })
