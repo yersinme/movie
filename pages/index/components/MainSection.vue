04ffde574f78be85f1d2f678ff65adfeaedcfb2a
Yersin <yersinme@gmail.com>
2025-07-04 18:30:58 +0500

    main section fix

--- pages/index/components/MainSection.vue
+++ pages/index/components/MainSection.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="main-section">
-    <AppHeader />
+    <AppHeader/>
 
     <div class="content">
       <h1 class="content-title">Insider</h1>
       <p class="content-text">2022 | Comedy horror | 1 Season</p>
-      <UiButton title="Watch now" />
+      <UiButton title="Watch now"/>
     </div>
   </section>
 </template>
@@ -16,17 +16,29 @@ import AppHeader from '~/components/AppHeader.vue'
 
 <style scoped lang="scss">
 .main-section {
+  position: relative;
   width: 100%;
   height: 455px;
   background-image: url('/images/main-banner.jpg');
-  background-size: cover;  padding: 32px 40px 53px;
+  background-size: cover;
+  padding: 32px 40px 53px;
   box-sizing: border-box;
 }
+
+.content {
+  position: absolute;
+  bottom: 23px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px
+}
+
 .content-title {
   color: #fff;
   font-size: 48px;
   font-weight: 600;
 }
+
 .content-text {
   color: #fff;
   font-size: 14px;
